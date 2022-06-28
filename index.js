944e72268913a7264a7ad4322c80ac3e54551883
Dac Nguyen <huyng97@yahoo.com>
2022-06-27 22:35:33 -0500

    projects section tabs, btns, styles

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-console.log('helllloooo');
+console.log('helllloooo console works');
 
 //this is for the tabs to open my projects
-function openCity(evt, cityName) {
+const openProject = (e, projectName) => {
   let i, tabcontent, tablinks;
+  
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
+  
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+  
+  document.getElementById(projectName).style.display = "block";
+  e.currentTarget.className += " active";
 }
+
+// closes project tab and sets button back to inactive
+const closeProject = (e) => {
+  e.parentElement.style.display='none';
+  let activeBtn = document.getElementById("tabBtns").getElementsByClassName("active");
+  activeBtn[0].classList.remove("active");
+}
+
+//sets default project when page loads
+document.getElementById("defaultProject").click();
 
 // header nav menu button on smaller screen
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
@@ -34,15 +47,14 @@ let localTheme = localStorage.getItem("theme");
 
 if (localTheme == "Light") {
   document.body.classList.remove("dark-theme");
-  colorThemeBtn.innerHTML = "Light Mode";
+  colorThemeBtn.innerHTML = "Dark Mode";
 } else if (localTheme == "Dark") {
   document.body.classList.add("dark-theme");
-  colorThemeBtn.innerHTML = "Dark Mode";
+  colorThemeBtn.innerHTML = "Light Mode";
 }
 
 colorThemeBtn.addEventListener('click', () => {
   document.body.classList.toggle("dark-theme");
-  
   if (colorThemeBtn.innerHTML === "Dark Mode") {
     colorThemeBtn.innerHTML = "Light Mode";
     localStorage.setItem("theme", "Light");
@@ -50,4 +62,5 @@ colorThemeBtn.addEventListener('click', () => {
     colorThemeBtn.innerHTML = "Dark Mode";
     localStorage.setItem("theme", "Dark");
   }
+  console.log('event:', colorThemeBtn.innerHTML, localStorage.getItem('theme'));
 });
