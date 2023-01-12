1e22b1d37e17e0cb1c94bbd9faed3c1a4919096f
dac <huyng97@yahoo.com>
2023-01-11 22:21:47 -0600

    project images

--- index.js
+++ index.js
@@ -1,36 +1,5 @@
 console.log('helllloooo console works');
 
-// //this is for the tabs to open my projects
-// const openProject = (e, projectName) => {
-//   let i, tabcontent, tablinks;
-  
-//   // hide all tabcontent elements
-//   tabcontent = document.getElementsByClassName("tabcontent");
-//   for (i = 0; i < tabcontent.length; i++) {
-//     tabcontent[i].style.display = "none";
-//   }
-  
-//   // hide all active tablinks btns
-//   tablinks = document.getElementsByClassName("tablinks");
-//   for (i = 0; i < tablinks.length; i++) {
-//     tablinks[i].className = tablinks[i].className.replace(" active", "");
-//   }
-  
-//   // show current tab, make tablink btn active
-//   document.getElementById(projectName).style.display = "block";
-//   e.currentTarget.className += " active";
-// }
-
-// // closes project tab and sets button back to inactive
-// const closeProject = (e) => {
-//   e.parentElement.style.display='none';
-//   let activeBtn = document.getElementById("tabBtns").getElementsByClassName("active");
-//   activeBtn[0].classList.remove("active");
-// }
-
-// //sets default project when page loads
-// document.getElementById("defaultProject").click();
-
 // header nav menu button on smaller screen
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('nav-menu')[0];
